290e74e047e28bf855c336f594a078338ba42bc7
Kargin Nikita <hei@spark-mail.ru>
2016-10-26 20:17:36 +0300

    Almost done

---
+++ src/TwoTFDemo.java
@@ -0,0 +1,14 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TwoTFDemo implements ActionListener {
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
